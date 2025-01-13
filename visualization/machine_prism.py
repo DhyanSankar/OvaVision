dbcec99cde84e92cf3b8bf53d5a63f58a6ba3854
Zenoson <Zenoson1234@gmail.com>
2025-01-13 09:32:31 -0500

    machine_prism init

--- visualization/machine_prism.py
+++ visualization/machine_prism.py
@@ -1,9 +1,11 @@
 class MachinePrison():
-        machine_array = []
+    machine_array = []
 
-        def __init__(self, dim_i, dim_j, dim_k, ): # number of machines
-            # define the machine array here, lowk forgot how to
-            return
+    def __init__(self, dimensions, distance, ex_machine_unit): # dimension, gap is a tuple, ex_machine_unit is an example of a machine unit
+        # define the machine array here, lowk forgot how to
+        self.machine_array = [[[ex_machine_unit for k in range(dimensions[2])] for j in range(dimensions[1])] for i in range(dimensions[0])]
+        # not sure if order is correct
+        return
         
         
         
