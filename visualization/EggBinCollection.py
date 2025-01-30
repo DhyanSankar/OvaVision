e68d10575cfa117a48e3a69f0d331363df981597
Zenoson <Zenoson1234@gmail.com>
2025-01-30 09:28:52 -0500

    changing implementation

---
+++ visualization/EggBinCollection.py
@@ -0,0 +1,12 @@
+class EggBinCollection():
+    bin_array = []
+    def __init__(self, dimensions, gap, bin_unit): # dimension, gap is a float of dist between top and bottom stuff, ex_machine_unit is an example of a machine unit
+        # dimensions = 
+        # define the machine array here, lowk forgot how to
+        self.machine_array = [[bin_unit for j in range(dimensions[1])] for i in range(dimensions[0])]
+        for i in range(dimensions[0]):
+            for j in range(dimensions[1]):
+                    self.bin_array[i][j].set_pos((i*gap,j*gap)) # this does not work 
+
+        # now you need to update the positions of each one, but im not sure if python thingy is copy by reference or value
+        # not sure if order is correct
