418468d40eb87b9d9149da25b3214c5413503d2e
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2025-02-04 09:02:40 -0500

    x motor changes

--- xMotor.py
+++ xMotor.py
@@ -4,6 +4,8 @@ from DRV8825 import DRV8825
 
 
 try:
+
+	# Has to change
 	Motor1 = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
 	Motor2 = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
 
