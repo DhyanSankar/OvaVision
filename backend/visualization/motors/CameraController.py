96ce6f05984c245fac017b6c3e2e9d605c0709fd
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2025-03-17 09:34:02 -0400

    Camera and incubator controller creation

---
+++ backend/visualization/motors/CameraController.py
@@ -0,0 +1,9 @@
+
+class CameraController:
+
+
+    def __init__(self, cameraModule):
+        self.camera = cameraModule;
+
+    ## Should figure out what operations the camera should do
+
---
+++ backend/visualization/motors/IncubatorLayerController.py
@@ -0,0 +1,29 @@
+import time
+import backend.visualization.motors.DRV8825 as DRV8825
+
+class IncubatorLayerController:
+	
+    # Going to be 4 layers typically I think?
+    numOfLayers = 4
+    layers = []
+    extended = []
+    def __init__(self, layers):
+        self.layers = layers
+        for i in range(4):
+            self.extended.append(False)
+        
+
+
+    def extend(self,layerNumber):
+        # Make the thing extend
+        self.extended[layerNumber] =True
+        pass
+
+    def retract(self,layerNumber):
+        # Make the thing retracted
+        self.extended[layerNumber] =False
+        pass
+
+    def reset(self):
+        for i in range(self.numOfLayers):
+            self.retract(i)
