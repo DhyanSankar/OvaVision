ed2974048af94639cab1ef1139c4bfa1ddea6a70
Dhyan Sankar <dhyansankar007@gmail.com>
2025-03-25 09:43:06 -0400

    current changes

--- backend/visualization/motors/xrzController.py
+++ backend/visualization/motors/xrzController.py
@@ -5,18 +5,27 @@ import threading
 import sys
 import os
 
-# Add the root directory to sys.path
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 sys.path.append(root_dir)
 
-from DRV8825 import DRV8825
+from visualization.motors.DRV8825 import DRV8825
+from visualization.motors.zMotor import zMotor
+from visualization.motors.rMotor import rMotor
+from visualization.motors.xController import XController
+from visualization.motors.rzController import RZController
+# from DRV8825 import DRV8825
+# from zMotor import zMotor
+# from rMotor import rMotor
 
-from DRV8825 import DRV8825
-from zMotor import zMotor
-from rMotor import rMotor
-from xMotor import xMotor
-from xController import XController
-from rzController import RZController
+
+# from DRV8825 import DRV8825
+
+# from DRV8825 import DRV8825
+# from zMotor import zMotor
+# from rMotor import rMotor
+# from xMotor import xMotor
+# from xController import XController
+# from rzController import RZController
 
 # from DRV8825 import DRV8825
 # from zMotor import zMotor
